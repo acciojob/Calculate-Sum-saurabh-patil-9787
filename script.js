5544542641fbb1429336ecffc747304e65ba40ff
saurabh-patil-9787 <154727373+saurabh-patil-9787@users.noreply.github.com>
2025-03-05 13:21:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+ <script>
+      const input1 = document.getElementById('n1')
+      const input2 = document.getElementById('n2')
+      const CalculateBtn = document.getElementById('sum_btn')
+      const displaySum = document.getElementById('sum')
+
+      CalculateBtn.addEventListener('click',calculateMysum)
+
+      function calculateMysum(){
+        let num1 = parseInt(input1.value)
+        let num2 = parseInt(input2.value)
+
+        displaySum.innerText = num1+num2
+      }
